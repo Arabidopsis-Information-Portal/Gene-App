a63d5ce441efcf8d90e164eb54271360972888f6
eriksf <erik@jcvi.org>
2015-08-04 19:10:59 -0400

    fixed wiredep issue, added clear/reset function, got search and table loading working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,16 +10,14 @@
 
     // Will be used to store initialized DataTables
     var chromosomeTable;
-    //var goTable;
+    var goTable;
 
     console.log('line');
 
-
     // Once the search button is clicked, retrieve the data
-    $('#searchButton').submit(function(event) {
+    $('#geneSearch').submit(function(event) {
+      event.preventDefault();
 
-      console.log(event);
-      /*
       console.log('search button pressed');
       // Reset UI
       $('#error', appContext).empty();
@@ -31,10 +29,9 @@
 
       // Saves user input as a dict parameter
       var params = {
-        'Identifier': $('input[name=identifierInput').val(),
+        'Identifier': $('input[name=identifierInput]').val(),
         'Output': 'all'
-
-      });
+      };
 
       console.log('api adama');
       // Calls API to retrieve chromosome data, using saved parameter
@@ -53,11 +50,28 @@
         showErrorMessage // Displays an error if Adama returns an exception
       );
 
-      */
+    });
+
+    $('#clearButton', appContext).on('click', function () {
+      // clear the gene field
+      $('input[name=identifierInput]', appContext).val('');
+
+      // clear the error section
+      $('#error', appContext).empty();
+
+      // clear the number of result rows from the tabs
+      $('#chromosome_num_rows', appContext).empty();
+      $('#go_num_rows', appContext).empty();
+
+      // clear the tables
+      $('#gene_chromosome', appContext).html('<h2>Please search for a geneID.</h2>');
+      $('#gene_go', appContext).html('<h2>Please search for a geneID.</h2>');
+
+      // select the about tab
+      $('a[href="#about"]', appContext).tab('show');
     });
 
     console.log('end function');
-
 
     // Creates a table to display chromosome data
     var showChromosomeData = function showChromosomeData(response) {
@@ -65,8 +79,8 @@
       // Stores API response
       var data = response.obj || response;
       data = data.result; // data.result contains an array of objects
-      console.log('fghu');
       // Creates a base table that the data will be stored in
+      console.log('Chromosome data: ' + JSON.stringify(data));
       $('#gene_chromosome', appContext).html(
         '<table width="100%" cellspacing="0" id="gene_chromosome-table"' +
         'class="table table-striped table-bordered table-hover">' +
@@ -75,21 +89,19 @@
 
         // Add data to table
         var field = '<td>' + 'locus_id' + '</td>';
-        var result = '<td>' + data[0].primaryIdentifier + '</td>';
+        var result = '<td>' + data[0].locus_id + '</td>';
         $('#gene_chromosome-data', appContext).append('<tr>' + field +
         result + '</tr>');
 
         field = '<td>' + 'chromosomeLocation.end' + '</td>';
-        result = '<td>' + data[1].chromosomeLocation.end + '</td>';
+        result = '<td>' + data[0]['chromosomeLocation.end'] + '</td>';
         $('#gene_chromosome-data', appContext).append('<tr>' + field +
         result + '</tr>');
 
         field = '<td>' + 'chromosomeLocation.start' + '</td>';
-        result = '<td>' + data[2].chromosomeLocation.start + '</td>';
+        result = '<td>' + data[0]['chromosomeLocation.start'] + '</td>';
         $('#gene_chromosome-data', appContext).append('<tr>' + field +
         result + '</tr>');
-
-
 
         // Converts normal table to DataTable
         chromosomeTable = $('#gene_chromosome-table', appContext).DataTable({
@@ -100,6 +112,8 @@
           dom: 'Rlfrtip', // Allows for user to reorder columns
           stateSave: true // Saves the state of the table between loads
         });
+
+        $('#chromosome_num_rows', appContext).html(' (' + chromosomeTable.data().length + ')');
       };
 
       // Creates a table to display go data
@@ -108,6 +122,7 @@
         // Stores API response
         var data = response.obj || response;
         data = data.result; // data.result contains an array of objects
+        console.log('GO data: ' + JSON.stringify(data));
         // Creates a base table that the data will be stored in
         $('#gene_go', appContext).html(
           '<table id="gene_go-table" width="100%" cellspacing="0"' +
@@ -129,6 +144,18 @@
             $('#gene_go-data', appContext).append('<tr>' + locusID +
             name + def + namespace + evidenceCode + '</tr>');
           }
+
+          // Converts normal table to DataTable
+          goTable = $('#gene_go-table', appContext).DataTable({
+            oLanguage: { // Overrides default text to make it more specific to this app
+              sSearch: 'Narrow results:',
+              sEmptyTable: 'No chromosomeental phosphorylation data available for this transcript id.'
+            },
+            dom: 'Rlfrtip', // Allows for user to reorder columns
+            stateSave: true // Saves the state of the table between loads
+          });
+
+          $('#go_num_rows', appContext).html(' (' + goTable.data().length + ')');
         };
 
         // Displays an error message if the API returns an error
