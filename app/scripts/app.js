6124b99254204f7905a6d10cb33777598922ce15
Iniyan Chezhian <ichezhia@ichezhia-osx.jcvi.org>
2015-08-04 16:11:24 -0400

    GO tab

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,7 @@
 
     // Will be used to store initialized DataTables
     var chromosomeTable,
-        goTable,
-        hotspotTable;
+    goTable;
 
     // Once the search button is clicked, retrieve the data
     //change to submit
@@ -19,40 +18,33 @@
       $('#error', appContext).empty();
 
       // Inserts loading text, will be replaced by table
+      console.log('loading');
       $('#gene_chromosome', appContext).html('<h2>Loading...</h2>');
       $('#gene_go', appContext).html('<h2>Loading...</h2>');
-      $('#gene_phenotype', appContext).html('<h2>Loading...</h2>');
 
       // Saves user-input as a parameter
       var params = {
-        Identifier: $('input[name=gene_transcript-input]').val()
-        Output: "all"
+        'Identifier': $('input[name=gene_transcript-input]').val(),
+        'Output': 'all'
       };
 
       // Calls API to retrieve chromosome data, using saved parameter
       Agave.api.adama.search(
-        {namespace: 'ichezhia', service: 'gene_by_geneid_v0.2',
-         queryParams: params},
+        {namespace: 'ichezhia-dev', service: 'gene_by_geneid_v0.2',
+        queryParams: params},
         showChromosomeData, // Displays retrieved data in a table
         showErrorMessage // Displays an error if Adama returns an exception
       );
+      console.log('adama');
+
+
 
       // Calls API to retrieve go data, using saved parameter
       Agave.api.adama.search(
-        //TODO
-        {namespace: 'ichezhia', service: 'go_by_geneid_v0.2',
-         queryParams: params},
+        {namespace: 'ichezhia-dev', service: 'go_by_geneid_v0.2',
+        queryParams: params},
         showgoData, // Displays retrieved data in a table
         showErrorMessage // Displays an error if Adama returns an exception
-      );
-
-      // Calls API to retrieve hotspot data, using saved parameter
-      Agave.api.adama.search(
-        //TODO
-        {namespace: 'ichezhia', service: 'phosphorylated_hotspots_v0.2',
-         queryParams: params},
-        showHotspotData, // Displays retrieved data in a table
-        showErrorMessage // Displays an error if Adama returns an exception.
       );
     });
 
@@ -62,132 +54,80 @@
       // Stores API response
       var data = response.obj || response;
       data = data.result; // data.result contains an array of objects
-
+      console.log('fghu');
       // Creates a base table that the data will be stored in
       $('#gene_chromosome', appContext).html(
         '<table width="100%" cellspacing="0" id="gene_chromosome-table"' +
         'class="table table-striped table-bordered table-hover">' +
-        '<thead><tr><th>Field</th><th>Result</th>' +
+        '<thead><th>Field</th><th>Result</th></thead>' +
         '<tbody id="gene_chromosome-data"></tbody></table>');
 
-      // Loops through each JSON object in the data
-      for (var i = 0; i < data.length; i++) {
-        // Saves data in strings to later be added to table
-        var field = '<td>' + data[i].peptide_sequence + '</td>';
-        var result = '<td>' + data[i].position_in_peptide + '</td>';
-        // Checks to see if a mass was provided, if so round it.
-        var peptideMass;
-        if (data[i].mass !== '') {
-          peptideMass = '<td>' + parseFloat(data[i].mass).toFixed(3) + '</td>';
-        } else {
-          peptideMass = '<td>' + 'not provided' + '</td>';
-        }
+        // Add data to table
+        var field = '<td>' + 'locus_id' + '</td>';
+        var result = '<td>' + data[0].primaryIdentifier + '</td>';
+        $('#gene_chromosome-data', appContext).append('<tr>' + field +
+        result + '</tr>');
 
-        // Dynamically adds saved data to the table
-        $('#gene_chromosome-data', appContext).append('<tr>' + peptideSeq +
-        peptidePos + modType  + peptideMass + '</tr>');
+        field = '<td>' + 'chromosomeLocation.end' + '</td>';
+        result = '<td>' + data[1].chromosomeLocation.end + '</td>';
+        $('#gene_chromosome-data', appContext).append('<tr>' + field +
+        result + '</tr>');
+
+        field = '<td>' + 'chromosomeLocation.start' + '</td>';
+        result = '<td>' + data[2].chromosomeLocation.start + '</td>';
+        $('#gene_chromosome-data', appContext).append('<tr>' + field +
+        result + '</tr>');
+
+
+
+        // Converts normal table to DataTable
+        chromosomeTable = $('#gene_chromosome-table', appContext).DataTable({
+          oLanguage: { // Overrides default text to make it more specific to this app
+            sSearch: 'Narrow results:',
+            sEmptyTable: 'No chromosomeental phosphorylation data available for this transcript id.'
+          },
+          dom: 'Rlfrtip', // Allows for user to reorder columns
+          stateSave: true // Saves the state of the table between loads
+        });
       }
+      // Creates a table to display go data
+      var showgoData = function showgoData(response) {
+        // Stores API response
+        var data = response.obj || response;
+        data = data.result; // data.result contains an array of objects
+        // Creates a base table that the data will be stored in
+        $('#gene_go', appContext).html(
+          '<table id="gene_go-table" width="100%" cellspacing="0"' +
+          'class="table table-striped table-bordered table-hover">' +
+          '<thead><tr><th>Locus ID</th><th>Name</th>' +
+          '<th>Def</th><th>Namespace</th><th>Evidence Code</th></tr></thead>' +
+          '<tbody id="gene_go-data"></tbody></table>');
+          // Loops through each JSON object in the data
+          for (var i = 0; i < data.length; i++) {
+            // Saves data in strings to later be added to table
+            var locusID = '<td>' + data[i].locus_id + '</td>';
+            var name = '<td>' + data[i].name + '</td>';
+            var def = '<td>' + data[i].def '</td>';
+            var namespace = '<td>' + data[i].namespace '</td>';
+            var evidenceCode = '<td>' + data[i].evidence_code '</td>';
 
-      // Converts normal table to DataTable
-      chromosomeTable = $('#gene_chromosome-table', appContext).DataTable({
-        oLanguage: { // Overrides default text to make it more specific to this app
-          sSearch: 'Narrow results:',
-          sEmptyTable: 'No chromosomeental phosphorylation data available for this transcript id.'
-        },
-        dom: 'Rlfrtip', // Allows for user to reorder columns
-        stateSave: true // Saves the state of the table between loads
-      });
+            // Dynamically adds saved data to the table
+            $('#gene_go-data', appContext).append('<tr>' + locusID +
+            name + def + namespace + evidenceCode + '</tr>');
+          }
 
-      // Add the number of rows to the tab name
-    $('#exp_num_rows', appContext).html(' (' + chromosomeTable.data().length + ')');
+      };
 
-    };
+        // Displays an error message if the API returns an error
+        var showErrorMessage = function showErrorMessage(response) {
+          console.log(response.obj.message);
+          $('#gene_error', appContext).html(
+            '<h4>API Error.' +
+            'See below:</h4><div class="alert alert-danger" role="alert">' +
+            response.obj.message + '</div>');
 
-/*
-#TODO
-    // Creates a table to display go data
-    var showgoData = function showgoData(response) {
-      // Stores API response
-      var data = response.obj || response;
-      data = data.result; // data.result contains an array of objects
-      // Creates a base table that the data will be stored in
-      $('#gene_go', appContext).html(
-        '<table id="gene_go-table" width="100%" cellspacing="0"' +
-        'class="table table-striped table-bordered table-hover">' +
-        '<thead><tr><th>Protein Position</th><th>13-mer Sequence</th>' +
-        '<th>Prediction Score</th></tr></thead>' +
-        '<tbody id="gene_go-data"></tbody></table>');
-      // Loops through each JSON object in the data
-      for (var i = 0; i < data.length; i++) {
-        // Saves data in strings to later be added to table
-        var proteinPos = '<td>' + data[i].position_in_protein + '</td>';
-        var sequence = '<td>' + data[i].thirteen_mer_sequence + '</td>';
-        // Rounds number
-        var predictionScore = '<td>' + parseFloat(data[i].prediction_score).toFixed(4) + '</td>';
-        // Dynamically adds saved data to the table
-        $('#gene_go-data', appContext).append('<tr>' + proteinPos +
-        sequence + predictionScore + '</tr>');
-      }
-      // Converts normal table to DataTable
-        goTable = $('#gene_go-table', appContext).DataTable({
-        oLanguage: { // Overrides default text to make it more specific to this app
-          sSearch: 'Narrow results:',
-          sEmptyTable: 'No go phosphorylation data available for this transcript id.'
-        },
-        dom: 'Rlfrtip', // Allows for user to reorder columns
-        stateSave: true // Saves the state of the table between loads
-      });
-      // Add the number of rows to the tab name
-      $('#pred_num_rows', appContext).html(' (' + goTable.data().length + ')');
-    };
-*/
+          };
 
-/*
-//TODO
-    // Creates a table to display hotspot data
-    var showHotspotData = function showHotspotData(response) {
-      // Stores API response
-      var data = response.obj || response;
-      data = data.result; // data.result contains an array of objects
-      // Creates a base table that the data will be stored in
-      $('#gene_hotspots', appContext).html(
-        '<table id="gene_hotspot-table" width="100%" cellspacing="0"' +
-        'class="table table-striped table-bordered table-hover">' +
-        '<thead><tr><th>Start Position</th><th>End Position</th>' +
-        '<th>Hotspot Sequence</th></tr></thead>' +
-        '<tbody id="gene_hotspot-data"></tbody></table>');
-      // Loops through each JSON object in the data
-      for (var i = 0; i < data.length; i++) {
-        // Saves data in strings to later be added to table
-        var start = '<td>' + data[i].start_position + '</td>';
-        var end = '<td>' + data[i].end_position + '</td>';
-        var sequence = '<td>' + data[i].hotspot_sequence + '</td>';
-        // Dynamically adds saved data to the table
-        $('#gene_hotspot-data', appContext).append('<tr>' + start +
-        end + sequence + '</tr>');
-      }
-      // Converts normal table to DataTable
-      hotspotTable = $('#gene_hotspot-table', appContext).DataTable({
-        oLanguage: { // Overrides default text to make it more specific to this app
-          sSearch: 'Narrow results:',
-          sEmptyTable: 'No hotspot data available for this transcript id.'
-        },
-        dom: 'Rlfrtip', // Allows for user to reorder columns
-        stateSave: true // Saves the state of the table between loads
-      });
-      // Add the number of rows to the tab name
-      $('#hot_num_rows', appContext).html(' (' + hotspotTable.data().length + ')');
-    };
-*/
+        });
 
-    // Displays an error message if the API returns an error
-    var showErrorMessage = function showErrorMessage(response) {
-      $('#gene_error', appContext).html(
-          '<h4>API Error.' +
-          'See below:</h4><div class="alert alert-danger" role="alert">' +
-           response.obj.message + '</div>');
-
-    };
-
-  });
-})(window, jQuery);
+      })(window, jQuery);
